87d2bbaf12450a10e68a6a56380ae455e96d3965
cytlin <cytlinadhiambo@gmail.com>
2022-05-26 12:42:32 +0300

    Commit 1

---
+++ app/src/main/java/com/example/newsapplication/CustomViewHolder.java
@@ -0,0 +1,22 @@
+package com.example.newsapplication;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomViewHolder extends RecyclerView.ViewHolder{
+    ImageView img_headline;
+    TextView text_title, text_source;
+    CardView cardView;
+    public CustomViewHolder(@NonNull View itemView) {
+        super(itemView);
+        text_title= itemView.findViewById(R.id.text_title);
+        text_source= itemView.findViewById(R.id.text_source);
+        img_headline= itemView.findViewById(R.id.img_headline);
+        cardView= itemView.findViewById(R.id.main_container);
+    }
+}
